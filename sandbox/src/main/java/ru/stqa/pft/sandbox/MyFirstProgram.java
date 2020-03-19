324cfadba73bd1cab5a5b6ea2707c7b451fba502
AReLinka <62117428+AReLinka@users.noreply.github.com>
2020-03-19 12:58:43 +0300

    Задание 2 - пункт 4. В класс point перенесен метод вычисления расстояния
    - выделила класс с методом расчёта квадрата числа
    - перенесла функцию вычисления расстояния в класс Point - метод distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,16 +4,12 @@ public class MyFirstProgram {
 	public static void main (String[] args)
 	{
 		//System.out.println("Hello, world!");
+
 		Point p1 = new Point(0, -3);
 		Point p2 = new Point(3, 1);
 
-		System.out.println("Расстояние между точками: p1(" + p1.x +", "+ p1.y + ") и p2("+ p2.x +", "+  p2.y +") = " + distance(p1, p2));
+		System.out.println("Расстояние между точками: p1(" + p1.x +", "+ p1.y + ") и p2("+ p2.x +", "+  p2.y +") = " + p1.distance(p2));
 	}
 
-	public static double square(double a) {
-		return a*a;
-	}
-	public static double distance(Point p1, Point p2) {
-		return Math.sqrt(square(p2.x-p1.x)+square(p2.y-p1.y));
-	}
+
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,8 +3,15 @@ package ru.stqa.pft.sandbox;
 public class Point {
   public double x;
   public double y;
+
   public Point (double x, double y){
     this.x = x;
     this.y = y;
   }
+  public double distance(Point p) {
+    Square l1 = new Square(p.x-this.x);
+    Square l2 = new Square(p.y-this.y);
+    return Math.sqrt(l1.square() + l2.square());
+  }
+
 }
