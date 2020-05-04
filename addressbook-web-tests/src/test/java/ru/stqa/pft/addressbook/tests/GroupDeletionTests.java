82fd977c24e6628c0a0a1ac0401a230b7a3f3991
AReLinka <AReLinka@users.noreply.github.com>
2020-05-04 19:22:16 +0300

    Добавлен контроль количества записей групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -10,13 +11,15 @@ public class GroupDeletionTests extends TestBase {
   // Удаление первой группы в списке
   public void testGroupDeletion() {
     app.getNavigatinHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("MyFirstGroup", null, null));
     }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
-
+    int after  = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before - 1);
   }
 
 }
