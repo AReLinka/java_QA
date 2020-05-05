ab8cb2741e0d892bfe047c783c2e4f52795c41ad
AReLinka <AReLinka@users.noreply.github.com>
2020-05-05 22:35:36 +0300

    Проверки сравнения списков групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,21 +1,64 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
-public class GroupCreationTests extends TestBase{
+public class GroupCreationTests extends TestBase {
 
   @Test
   // Создание новой группы
   public void testGroupCreation() {
     app.getNavigatinHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("MyFirstGroup", null, null));
+    GroupData group = new GroupData("MyFirstGroup", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+
+    /* //Варианты вычислений максимального идентификатора
+    //вар. 1 - цикл
+    int max = 0;
+    for (GroupData g : after) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    group.setId(max);*/
+
+  /* // вар. 2 - сравниватель
+   Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+      @Override
+      public int compare(GroupData o1, GroupData o2) {
+        return Integer.compare(o1.getId(), o2.getId());
+      }
+    };
+    int max1 = after.stream().max(byId).get().getId();
+    group.setId(max1);
+    */
+
+    /* //вар. 3 лямбда выражение - анонимная функция
+     Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+    int max1 = after.stream().max(byID).get().getId();
+    group.setId(max1);
+    */
+
+    /* // самый короткий вариант
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+     */
+
+    before.add(group);
+
+    Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
+    //Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
   }
 
 }
