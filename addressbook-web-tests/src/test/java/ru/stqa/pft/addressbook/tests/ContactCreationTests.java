7113a27a90a4634f0c37f9eb10171967f4602145
AReLinka <AReLinka@users.noreply.github.com>
2020-05-11 19:08:48 +0300

    Поиск максимального ID элемента в коллекции Контактов лямбда-выражением

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -23,15 +23,9 @@ public class ContactCreationTests extends TestBase {
     ContactData contact = new ContactData("Alina", "Sandyga", "Saint-Petersburg", "89111232233", "1@1.ru", "MyFirstGroup");
     app.getContactHelper().createContact(contact, true);
     List<ContactData> after = app.getContactHelper().getContactList();
-    Assert.assertEquals(after.size(), before.size() + 1 );
+    Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData c: after){
-      if (c.getId() > max){
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
