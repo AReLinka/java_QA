f963b7b7a39ca7485a5969edd528f0fffbcf1727
AReLinka <AReLinka@users.noreply.github.com>
2020-05-17 17:21:42 +0300

    Генерация тестовых данных для контактов (csv, xml, json)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,19 +1,30 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.io.File;
 import java.util.Objects;
 
+@XStreamAlias("contact")
 public class ContactData {
+  @XStreamOmitField
   private int id = Integer.MAX_VALUE;
+  @Expose
   private String name;
+  @Expose
   private String lastname;
+  @Expose
   private String address;
 
+  @Expose
   private String homePhone;
   private String mobilePhone;
   private String workPhone;
   private String allPhones;
 
+  @Expose
   private String firstMail;
   private String secondMail;
   private String thirdMail;
