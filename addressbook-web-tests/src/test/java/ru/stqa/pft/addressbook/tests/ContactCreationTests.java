051d7dceca42d51f58d0c5c980a01aa84fd789a5
AReLinka <AReLinka@users.noreply.github.com>
2020-05-17 17:34:46 +0300

    Загрузка тестовых данных для контактов из файла .csv

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,20 +25,32 @@ public class ContactCreationTests extends TestBase {
     }
   }
 
-  @Test
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new ContactData().withName(split[0])
+              .withLastname(split[1]).withAddress(split[2])
+              .withFirstMail(split[3]).withHomePhone(split[4])
+              .withGroup("MyFirstGroup")});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
   // Создание нового контакта
-  public void testContactCreation() {
+  public void testContactCreation(ContactData contact) {
     app.goTo().HomePage();
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/stru.jpg");
-    ContactData contact = new ContactData()
-            .withName("Alina").withLastname("Sandyga").withAddress("Saint-Petersburg")
-            .withHomePhone("89111232233").withFirstMail("1@1.ru").withGroup("MyFirstGroup")
-            .withPhoto(photo);
     app.contact().create(contact, true);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
     assertThat(after, equalTo(
-            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 }
