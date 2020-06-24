a61b55eca385e49e6c69f3af5e70eb1789dc3768
AReLinka <AReLinka@users.noreply.github.com>
2020-06-24 22:59:54 +0300

    Прямое взаимодействие с сервером по протоколу HTTP. Тест логина

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -38,4 +38,12 @@ public class ApplicationManager {
     wd.get(properties.getProperty("web.baseUrl"));
   }
 public void stop() {wd.quit();}
+
+  public HttpSession newSession() {
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
 }
